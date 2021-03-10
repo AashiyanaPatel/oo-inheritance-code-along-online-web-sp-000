c96fe34d0997a6d936eb062ae3bf9322dc00c1df
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-10 02:18:35 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,10 +1,10 @@
 class Vehicle
 
-  attr_accessor :wheel_size, :wheel_number
+  attr_accessor :wheel_number, :wheel_size
 
-  def intitalize(wheel_size, wheel_number)
-    @wheel_size = wheel_size
+  def intitalize(wheel_number, wheel_size)
     @wheel_number = wheel_number
+    @wheel_size = wheel_size
   end
 
   def go
